e213459ff2bb6c119f773e45d0f2307f31203947
AaryaDesai1 <143753050+AaryaDesai1@users.noreply.github.com>
2023-09-08 15:24:45 +0000

    Polars does not have iloc

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """Importing module pandas for my function."""
 import polars as pl
 df = pl.read_csv("iris.csv")
+print(df)
 
 def avg_88(data):
     """"This is a mean function."""
     result = data.mean()
     return result
 
-print(avg_88(df.iloc[:, 1]))
+print(avg_88(df.select('sepal.length')))
