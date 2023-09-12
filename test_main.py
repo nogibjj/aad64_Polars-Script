caf5adccee135ae3443174c3a5b18c942b8781a6
AaryaDesai1 <143753050+AaryaDesai1@users.noreply.github.com>
2023-09-12 15:35:47 -0400

    Adding color to graph

--- test_main.py
+++ test_main.py
@@ -20,12 +20,13 @@ testing_main_med()
 testing_main_std()
 
 # Testing Usage of Visualization function:
-data = {'x': [1, 2, 3, 4, 5], 'y': [2, 3, 5, 7, 11]}
-testing_data2 = pl.DataFrame(data)
+#data = {'x': [1, 2, 3, 4, 5], 'y': [2, 3, 5, 7, 11]}
+testing_data2 = pl.read_csv('iris.csv')
 
 visualize_data(testing_data2, 
-               'x', 
-               'y', 
+               'sepal.length', 
+               'sepal.height', 
+               'variety',
                title='Line Plot Example', 
                xlabel='X-axis', 
                ylabel='Y-axis')
