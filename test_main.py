6e3d498b8c1dbb25341c9a12381d84a4424fabe7
AaryaDesai1 <143753050+AaryaDesai1@users.noreply.github.com>
2023-09-12 15:20:51 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,22 +1,23 @@
-from main import (visualize_data)
+from main import (average, 
+                  med, 
+                  standard_deviation, 
+                  visualize_data)
 import polars as pl
 
 dataset = {"Values": [1, 3, 5, 7, 9]}
 testing_data = pl.DataFrame(dataset)
-#average, med, standard_deviation, 
+def testing_main_avg():
+    assert average(testing_data['Values']) == 5
 
-#def testing_main_avg():
-#    assert average(testing_data.select('Values')) == 5
+def testing_main_med():
+    assert med(testing_data['Values']) == 5
 
-#def testing_main_med():
-#    assert med(testing_data.select('Values')) == 5
+def testing_main_std():
+    assert standard_deviation(testing_data['Values']) == 3.1622776601683795
 
-#def testing_main_std():
-#    assert standard_deviation(testing_data.select('Values')) == 3.1622776601683795
-
-#testing_main_avg()
-#testing_main_med()
-#testing_main_std()
+testing_main_avg()
+testing_main_med()
+testing_main_std()
 
 # Testing Usage of Visualization function:
 data = {'x': [1, 2, 3, 4, 5], 'y': [2, 3, 5, 7, 11]}
